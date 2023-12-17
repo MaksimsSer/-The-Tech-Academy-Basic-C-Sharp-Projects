d54990928635c6105f03f0b0fc3712e4c666369f
Maksims Sersnovs <maksimssir@gmail.com>
2023-12-17 18:33:17 +0000

    WelcometoAcmeAccountingSystems

---
+++ WelcometoAcmeAccountingSystems/WelcometoAcmeAccountingSystems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Print the welcome message to the console
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remember, we’re “accounting” on you!");
+        Console.ReadLine();
+    }
+}
