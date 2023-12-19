d65c09c7290a3889293c4da2a9afe4ed2c824726
Maksims Sersnovs <maksimssir@gmail.com>
2023-12-19 18:49:43 +0000

    CarInsuranceProgram
    Car Insurance program

---
+++ CarInsuranceApprovalProgram/CarInsuranceApprovalProgram/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceApprovalProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car insurance Approval Program\n");
+
+            //Asking questions
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you evere had a DUi? (true/false)");
+            bool hasDUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+            //Checking qualification rules
+            bool qualifiesForInsurance = (age > 15) && (!hasDUI) && (speedingTickets <= 3);
+
+            //printing results
+            Console.WriteLine("\nQualifies for car insurance:" + qualifiesForInsurance);
+
+            Console.ReadLine();
+        }
+    }
+}
