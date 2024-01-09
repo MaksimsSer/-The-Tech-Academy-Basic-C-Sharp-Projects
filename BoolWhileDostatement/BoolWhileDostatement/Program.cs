2da194b1fe477f9ded8bc199b643b3ac9af7d836
Maksims Sersnovs <maksimssir@gmail.com>
2024-01-09 17:19:37 +0000

    Console app Assignment
    Boolean do/while statement

---
+++ BoolWhileDostatement/BoolWhileDostatement/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoolWhileDostatement
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Boolean comparison using while loop
+            Console.WriteLine("Boolean comparison using while loop:");
+            int number = 7;
+
+            while (number < 15) //While the number is less than 5
+            {
+                Console.WriteLine($"Number is {number}");
+                number++; //Insert number by 1
+            }
+            Console.WriteLine();
+
+
+            //Boolean comparison using do while loop
+            Console.WriteLine("Boolean comparison using do while loop:");
+            int counter = 0;
+
+            do
+            {
+                Console.WriteLine($"Counter is {counter}");
+                counter++; //Increment counter by 1
+            }
+            while (counter < 5); // Do it qt least once while the counter is less than 5
+            Console.ReadLine();
+        }
+    }
+}
