e93e92c0e8c4392c3ae6d9f0d6724aead4f048a1
Maksims Sersnovs <maksimssir@gmail.com>
2024-02-12 16:56:16 +0000

    Main Method Assignment

---
+++ MainMethodAssignment/MainMethodAssignment/MathMethods2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace MainMethodAssignment
+{
+    public class MathMethods2
+    {
+        //First method that takes an integer
+        public int MathOp(int number)
+        {
+            return number + 4;
+        }
+        //Second method that takes a decemal
+        public int MathOp(decimal number)
+        {
+            //Convert the decemal to an int to perform math operation
+            int newnum = Convert.ToInt32(number);
+            return newnum * 5;
+        }
+        //Third MathOp method that takes a string
+        public int MathOp(string number)
+        {
+            //Convert the string to an int to perform math operation
+            int newnum = Convert.ToInt32(number);
+            return newnum % 3;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathMethods2 class
+            MathMethods2 math1 = new MathMethods2();
+            //Call the first method with an int parameter
+            Console.WriteLine(math1.MathOp(10));
+            //Call the second method with a decimal parameter
+            Console.WriteLine(math1.MathOp(10.3m));
+            //Call the third method with a string parameter
+            Console.WriteLine(math1.MathOp("10"));
+            Console.ReadLine();
+        }
+        
+    }
+}
