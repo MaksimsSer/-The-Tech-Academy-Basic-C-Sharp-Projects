f8ffd64cd1b2d42dac12515a3298bcff29853873
Maksims Sersnovs <maksimssir@gmail.com>
2024-02-12 18:19:47 +0000

    Math operation

---
+++ MathOperation/MathOperation/MathOperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace MathOperation
+{
+    //Define a class named MathOperation
+    class MathOperation
+    {
+        //Define a void method that takes two integers as parameters
+        public void PerformMathOperation(int num1, int num2)
+        {
+            //Perform a math operation on the first integer
+            //For demonstration, lets just add 10 to the first number
+            int result = num1 + 10;
+
+            // Display second integer to the screen
+            Console.WriteLine($"The second integer is : {num2}");
+        }
+    }
+}
---
+++ MathOperation/MathOperation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace MathOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the MathOperation class
+            MathOperation mathOperation = new MathOperation();
+            //Call the method in the class, passing in two numbers
+            mathOperation.PerformMathOperation(5, 8);
+            //Call the method in the class, specifying the parameters
+            mathOperation.PerformMathOperation(num1: 8, num2: 10);
+            //Keep the console open untill a key is pressed
+            Console.ReadKey();
+        }
+    }
+}
