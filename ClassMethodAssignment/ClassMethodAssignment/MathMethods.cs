f94c247b7812304a171222d353cc1dc7c21a5918
Maksims Sersnovs <maksimssir@gmail.com>
2024-02-13 15:45:07 +0000

    ClassMethodAssignment

---
+++ ClassMethodAssignment/ClassMethodAssignment/MathMethods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    public class MathMethods
+    {
+        public static int Heal(int magic)
+        {
+            int hitpoints = magic + 10;
+            return hitpoints;
+        }
+
+        public static int Defense(int defense)
+        {
+            int enemyStr = 7;
+            int weaponMt = 11;
+            int triangleBonus = -1;
+            int critCoeff = 1;
+            int damage = (enemyStr + (weaponMt + triangleBonus) - defense) * critCoeff;
+            return damage;
+        }
+
+        public static int Attack(int strength)
+        {
+            int enemyDef = 4;
+            int weaponMt = 8;
+            int triangleBonus = 1;
+            int critCoeff = 3;
+            int damage = (strength + (weaponMt + triangleBonus) - enemyDef) * critCoeff;
+            return damage;
+        }
+    }
+}
---
+++ ClassMethodAssignment/ClassMethodAssignment/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number:");
+            int magicNumber = Convert.ToInt32(Console.ReadLine());
+            int healedHitpoints = MathMethods.Heal(magicNumber);
+            Console.WriteLine("Healed Hitpoints: " + healedHitpoints);
+            Console.ReadLine();
+
+            Console.WriteLine("Input a phrase:");
+            string input = Console.ReadLine();
+            int count = CountLetterI(input);
+            Console.WriteLine("Count of 'I's: " + count);
+            Console.WriteLine("Doubled count of 'I's: " + (count * 2));
+            Console.ReadLine();
+
+            Console.WriteLine("Input another phrase: ");
+            string input2 = Console.ReadLine();
+            int phraseLength = input2.Length;
+            Console.WriteLine("This phrase is " + phraseLength + " characters long");
+            Console.ReadLine();
+
+            Console.WriteLine("Input another phrase:");
+            string input3 = Console.ReadLine();
+            int iCount = CountLetterI(input3);
+            Console.WriteLine("This phrase has " + iCount + " 'i's in it.");
+            Console.ReadLine();
+        }
+
+        static int CountLetterI(string input)
+        {
+            // Count occurrences of 'I' or 'i'
+            int count = 0;
+            foreach (char c in input)
+            {
+                if (c == 'I' || c == 'i')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
