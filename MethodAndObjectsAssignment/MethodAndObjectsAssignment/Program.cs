20cecc47b0d42139574685de0019c8039157f8cb
Maksims Sersnovs <maksimssir@gmail.com>
2024-02-19 19:10:07 +0000

    MethodAndObjectAssignment

---
+++ MethodAndObjectsAssignment/MethodAndObjectsAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MethodAndObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inside of thhe Main method, instantiate and initialize an Employoee object with 
+            //the first name "Sample" and the last name "Student"
+            Employee sam = new Employee() { FirstName = "Sample", LastName = "Student" };
+            //Call the superclass method SayName() on the Employee object
+            sam.SayName();
+            Console.ReadLine();
+        }
+    }
+}
