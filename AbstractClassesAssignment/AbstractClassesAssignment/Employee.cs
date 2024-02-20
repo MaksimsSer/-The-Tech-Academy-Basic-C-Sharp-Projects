65ddd2f39a8ea4e5ac05d2b833147fb5d98c2051
Maksims Sersnovs <maksimssir@gmail.com>
2024-02-20 17:37:40 +0000

    Abstract Classes Assignment

---
+++ AbstractClassesAssignment/AbstractClassesAssignment/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbstractClassesAssignment
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine($"Employee Name: {firstName} {lastName}");
+        }
+    }
+}
---
+++ AbstractClassesAssignment/AbstractClassesAssignment/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace AbstractClassesAssignment
+{
+   public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        // Give it the method SayName();
+        public abstract void SayName();
+    }
+}
