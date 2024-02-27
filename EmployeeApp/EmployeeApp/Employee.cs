52a9cf21de16ed84264273a3b9098beda556a871
Maksims Sersnovs <maksimssir@gmail.com>
2024-02-27 15:17:51 +0000

    EmployeeApp
    "=="
    "!="

---
+++ EmployeeApp/EmployeeApp/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EmployeeApp
+{
+    //Define the Employee class
+    public class Employee
+    {
+        //Properies of the Employee class
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Constructor to initialize Employee Object
+        public Employee(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        //Overloading the "==" operator to compare Employee objects by their ID property
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            //Check if both objects are null or if their reference is the same
+            if (ReferenceEquals(emp1, emp2))
+                return true;
+            //Check if one of the objects is null
+            if (emp1 is null || emp2 is null)
+                return false;
+            //Compare the Id property of both objects
+            return emp1.Id == emp2.Id;
+        }
+
+        //Overloading the "!=" operator to complement the overload "==" operator
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            //Use the inverse of the overloaded "==" operator
+            return !(emp1 == emp2);
+        }
+    }
+
+}
---
+++ EmployeeApp/EmployeeApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EmployeeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating instances of Employee class
+            Employee emp1 = new Employee(1, "John", "Smith");
+            Employee emp2 = new Employee(2, "Jane", "Doe");
+            Employee emp3 = new Employee(1, "John", "Smith"); //Creating a duplicate of emp1
+
+            //Testing the overloaded "==" operator 
+            Console.WriteLine("Comparison Results: ");
+            Console.WriteLine($"emp1 == emp2: {emp1 == emp2}"); // False because the Id of emp1 (1) is not equal to the Id of emp2
+            Console.WriteLine($"emp1 == emp3: {emp1 == emp3}"); // True because both emp1 and emp3 have the same Id (1).
+
+            //Testing the overload "!=" operator
+            Console.WriteLine($"emp1 != emp2: {emp1 != emp2}"); //True because emp1 and emp2 have different Ids.
+            Console.WriteLine($"emp1 != emp3: {emp1 != emp3}"); // False because emp1 and emp3 have the same Id.
+
+            Console.ReadLine();
+        } 
+    }
+}
