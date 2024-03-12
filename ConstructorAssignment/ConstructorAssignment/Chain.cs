7f12d6721545e5fae1407e5536523c42930ffc94
Maksims Sersnovs <maksimssir@gmail.com>
2024-03-12 17:40:27 +0000

    ConstructorAssignment
    Create a const variable.
    
    Create a variable using the keyword “var.”
    
    Chain two constructors together.

---
+++ ConstructorAssignment/ConstructorAssignment/Chain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    public class Chain
+    {
+        string Name { get; set; }
+        float Num { get; set; }
+
+        public Chain(float num, string name)
+        {
+            Name = name;
+            Num = num;
+        }
+        //Constructor if one parameter, a float, is provided
+        public Chain(float num1) : this(num1, "Maksimus")
+        {
+            Console.WriteLine(num1 + "and your default name is " + Name);
+        }
+        //Constructor if one parameter, a string, is provided
+        public Chain(string name1) : this(42, name1)
+        {
+            Console.WriteLine(name1 + " and your default favorite number is " + Num); 
+        }
+
+    }
+}
