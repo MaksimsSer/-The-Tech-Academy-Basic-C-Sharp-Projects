4039c31b338525149a8d1377e6d0209b2da68857
Maksims Sersnovs <maksimssir@gmail.com>
2024-03-19 15:12:57 +0000

    Try/Catch
    Ask the user for their age.
    
    Display the year the user was born.
    
    Exceptions must be handled using “try/catch.”
    
    Display appropriate error messages if the user enters zero or negative numbers.
    
    Display a general message if an exception was caused by anything else.

---
+++ TryCatch/TryCatch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Ask the user for their age
+                Console.WriteLine("Please enter your age: ");
+                int age = int.Parse(Console.ReadLine());
+
+                //Check if age is negative or zero
+                if(age<=0)
+                {
+                    throw new ArgumentException("Age must be positive number.");
+                }
+                //Calculate the birth year
+                int birthYear = DateTime.Now.Year - age;
+                //Display the year the use was born
+                Console.WriteLine($"You were born in the year {birthYear}");
+            }
+            catch (FormatException)
+            {
+                //Display error message for invalid input (non-integer)
+                Console.WriteLine("Error: Please enter a vaild integer for age.");
+            }
+            catch (ArgumentException ex)
+            {
+                //Display error message for zero or negative age
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            catch (Exception)
+            {
+                //Display general error message for other exeptions
+                Console.WriteLine("An error occurred. Please try again");
+            }
+            Console.ReadLine();
+        }
+    }
+}
